dad7cf15d9eccfec6ca57de48efc0922a9811d3a
joel marques de lima <jotamantys@gmail.com>
2020-07-08 02:59:49 -0300

    implementado model e controler para as rotas da API

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
+import modelSchema from "./gradeModel.js"
 
 const db = {};
 db.mongoose = mongoose;
 db.url = process.env.MONGODB;
+db.gradeModel = modelSchema; 
 
 export { db };
